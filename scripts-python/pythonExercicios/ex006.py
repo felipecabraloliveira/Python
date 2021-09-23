b1ad9c14e3776afd493d16b917fbc1d3b6c3dc45
felipecabraloliveira <felipecabral33@gmail.com>
2021-09-22 21:34:01 -0300

    Update ex006.py

--- scripts-python/pythonExercicios/ex006.py
+++ scripts-python/pythonExercicios/ex006.py
@@ -5,6 +5,8 @@
 # meu código
 
 n = int(input('Digite um número: '))
-print('Número digitado: {} \nDobro: {} \nTriplo: {} \nRaiz quadrada: {}'.format(n,n*2,n*3,n**(1/2)))
+print('Número digitado: {} \nDobro: {} \nTriplo: {} \nRaiz quadrada: {:.2f}'.format(n,n*2,n*3,n**(1/2)))
 
-# Correção
+# Correção - OK
+
+# Pode ser utilizado uma função interna para a raiz quadrada pow(n,(1/2))
