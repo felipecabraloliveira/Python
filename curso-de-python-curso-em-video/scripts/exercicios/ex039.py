dea095c8ac9ab4c06c303546ce0d8a21ebcde3dd
felipecabraloliveira <felipecabral33@gmail.com>
2021-10-04 14:21:55 -0300

    Exercícios 039 - 040

--- curso-de-python-curso-em-video/scripts/exercicios/ex039.py
+++ curso-de-python-curso-em-video/scripts/exercicios/ex039.py
@@ -1,1 +1,30 @@
 # Exercício 039 do curso de Python - Curso em vídeo
+
+# Faça um programa que leia o ano de nascimento de um jovem e informe, de acordo com a sua idade,
+# se ele ainda vai se alistar ao serviço militar, se é a hora exata de se alistar ou se
+# já passou do tempo do alistamento.
+# Seu programa também deverá mostrar o tempo que falta ou que passou do prazo.
+from datetime import date
+cores = {'blue': '\033[1;34m', 'red': '\033[1;31m', 'yellow': '\033[1;33m', 'g': '\033[1;32m',
+         'magenta': '\033[1;35m', 'peb': '\033[1;30;107m', 'bep': '\033[1;97;40m', 'remove': '\033[0;0m'}
+print('*' * 30)
+print(' ' * 9 + 'Alistamento!')
+print('*' * 30)
+ano = int(input('Digite o ano de nascimento? '))
+idade = 2021 - ano
+data = date.today()
+y = data.strftime('%Y')
+print('Você nasceu em {} e completa {} anos em {}.'.format(ano, idade, y))
+if idade > 18:
+    tempo = 2021 - (ano + 18)
+    print('O período de alistamento já passou!')
+    print('{}O ano do seu alistamento foi em {}, você está atrasado há {} ano(s).{}'
+          .format(cores['red'], int(y) - tempo, tempo, cores['remove']))
+elif idade == 18:
+    tempo = 0
+    print('{} É a hora exata de se alistar!{}'.format(cores['blue'], cores['remove']))
+else:
+    tempo = (ano + 18) - 2021
+    print('Você deve se alistar em breve!')
+    print('{}O ano do seu alistamento é {}, faltam apenas {} ano(s) para se alistar!{}'
+          .format(cores['g'], ano + 18, tempo, cores['remove']))
--- curso-de-python-curso-em-video/scripts/exercicios/ex040.py
+++ curso-de-python-curso-em-video/scripts/exercicios/ex040.py
@@ -1,2 +1,24 @@
 # Exercício 040 do curso de Python - Curso em vídeo
 
+#  Crie um programa que leia duas notas de um aluno e calcule sua média,
+#  mostrando uma mensagem no final, de acordo com a média atingida:
+# – Média abaixo de 5.0: REPROVADO
+# – Média entre 5.0 e 6.9: RECUPERAÇÃO
+# – Média 7.0 ou superior: APROVADO
+from time import sleep
+cores = {'blue': '\033[1;34m', 'red': '\033[1;31m', 'yellow': '\033[1;33m', 'g': '\033[1;32m',
+         'magenta': '\033[1;35m', 'peb': '\033[1;30;107m', 'bep': '\033[1;97;40m', 'remove': '\033[0;0m'}
+print('*' * 30)
+print(' ' * 9 + 'Calculando Média!')
+print('*' * 30)
+n1 = float(input('Digite a primeira nota: '))
+n2 = float(input('Digite a segunda nota: '))
+print('Analisando a média...')
+sleep(1)
+med = (n1 + n2) / 2
+if med >= 7.0:
+    print('Sua média foi de {:.2f}, {}Aprovado!{}'.format(med, cores['blue'], cores['remove']))
+elif 5 <= med <= 6.99:
+    print('Sua média foi de {:.2f}, {}Recuperação!{}'.format(med, cores['magenta'], cores['remove']))
+else:
+    print('Sua média foi de {:.2f}, {}Reprovado!{}'.format(med, cores['red'], cores['remove']))
