5d10ab7ae9714737ee5a1ea58de2f95db0e8943a
felipecabraloliveira <felipecabral33@gmail.com>
2021-10-11 21:15:52 -0300

    Correção 038

--- curso-de-python-curso-em-video/scripts/exercicios/ex038.py
+++ curso-de-python-curso-em-video/scripts/exercicios/ex038.py
@@ -10,16 +10,15 @@ cores = {'blue': '\033[1;34m', 'red': '\033[1;31m', 'yellow': '\033[1;33m', 'g':
 print('*' * 30)
 print(' ' * 4 + 'Comparando números!')
 print('*' * 30)
-
 v1 = int(input('Digite o primeiro valor? '))
 v2 = int(input('Digite o segundo valor? '))
-
 print('Primeiro valor digitado foi o {} e o segundo valor digitado foi o {}.'.format(v1, v2))
 print('Comparando...')
-sleep(2)
+sleep(1)
 if v1 > v2:
     print('O {}primeiro{} valor é MAIOR! {} é maior do que {}.'.format(cores['g'], cores['remove'], v1, v2))
 elif v2 > v1:
     print('O {}segundo{} valor é MAIOR! {} é maior do que {}.'.format(cores['blue'], cores['remove'], v2, v1))
 else:
     print('Não existe valor maior, os dois são IGUAIS! {} é igual a {}.'.format(v1, v2))
+# Correção - Ok
