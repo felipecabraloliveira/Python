10327e8e6a636d21ab1259957376419c24aa444c
felipecabraloliveira <felipecabral33@gmail.com>
2021-10-13 19:57:12 -0300

    Correção 045

--- curso-de-python-curso-em-video/scripts/exercicios/ex045.py
+++ curso-de-python-curso-em-video/scripts/exercicios/ex045.py
@@ -49,3 +49,51 @@ else:
     dcomp = jogada
     djogada = jogada
 print('Computador: {} x você: {}'.format(dcomp, djogada))
+
+# Correção
+from random import randint
+itens = ('Pedra', 'Papel', 'Tesoura')
+computador = randint(0, 2)
+print('O computador escolheu{}'. format(computador))
+print('''Suas opções:
+[ 0 ] PEDRA
+[ 1 ] PAPEL
+[ 2 ] TESOURA''')
+jogador = int(input('Qual é a sua jogada? '))
+print('JO')
+sleep(0.5)
+print('KEN')
+sleep(1)
+print('PO!!!')
+print('-='*11)
+print('Computador jogou {}'. format(itens[computador]))
+print('Jogador jogou {}'. format(itens[jogador]))
+print('-='*11)
+if computador == 0: # Pedra
+    if jogador == 0:
+        print('Empate')
+    elif jogador == 1:
+        print('Jogador Vence!')
+    elif jogador == 2:
+        print('Computador Vence!')
+    else:
+        print('{}Opção inválida!{}'.format(cores['red'], cores['remove']))
+elif computador == 1: # Papel
+    if jogador == 0:
+        print('Computador Vence!')
+    elif jogador == 1:
+        print('Empate')
+    elif jogador == 2:
+        print('Jogador Vence!')
+    else:
+        print('{}Opção inválida!{}'.format(cores['red'], cores['remove']))
+elif computador == 2: # Tesoura
+    if jogador == 0:
+        print('Jogador Vence!')
+    elif jogador == 1:
+        print('Computador Vence!')
+    elif jogador == 2:
+        print('Empate')
+    else:
+        print('{}Opção inválida!{}'.format(cores['red'], cores['remove']))
+        
