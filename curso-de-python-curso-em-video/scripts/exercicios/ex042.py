2d3f9c67d63a48f99fd3e3db47851d08c696ccea
felipecabraloliveira <felipecabral33@gmail.com>
2021-10-13 19:30:09 -0300

    Correção 042

--- curso-de-python-curso-em-video/scripts/exercicios/ex042.py
+++ curso-de-python-curso-em-video/scripts/exercicios/ex042.py
@@ -12,16 +12,17 @@ cores = {'blue': '\033[1;34m', 'red': '\033[1;31m', 'yellow': '\033[1;33m', 'g':
 print('*' * 30)
 print(' ' * 5 + 'Tipos de Triângulo!')
 print('*' * 30)
-
 r1 = float(input('Digite o primeiro segmento: '))
 r2 = float(input('Digite o segundo segmento: '))
 r3 = float(input('Digite o terceiro segmento: '))
-
 if r1 < r2 + r3 and r2 < r1 + r3 and r3 < r1 + r2:
-    print('Os segmentos acima podem formar um triângulo.')
-    if r1 == r2 and r1 == r3 and r2 == r3:
+    print('Os segmentos acima podem formar um triângulo.', end='')
+    if r1 == r2 and r2 == r3:
         print('O triângulo formado é um EQUILÁTERO!')
-    elif r1 == r2 or r2 == r3:
+    elif r1 != r2 != r3 != r1:
+        print('O triângulo formado é um ESCALENO!')
+    else:
         print('O triângulo formado é um ISÓSCELES')
 else:
     print('Os segmentos não podem formar triângulo.')
+# Correção - Ok
