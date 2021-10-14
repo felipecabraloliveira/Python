efeda7705bcc07d1f577d006d780c2aa955e08e1
felipecabraloliveira <felipecabral33@gmail.com>
2021-10-14 19:36:32 -0300

    Exercício 051

---
+++ curso-de-python-curso-em-video/scripts/exercicios/ex051.py
@@ -0,0 +1,15 @@
+# Exercício 051 do curso de Python - Curso em vídeo
+
+#  Desenvolva um programa que leia o primeiro termo e a razão de uma PA.
+#  No final, mostre os 10 primeiros termos dessa progressão.
+
+# Meu Código
+print('*' * 32)
+print(' ' * 6 + '10 Termos de uma PA!')
+print('*' * 32)
+pri = int(input('Primeiro termo: '))
+razao = int(input('Razão: '))
+for c in range(pri, razao * 10 , razao):
+    print(c, end= ' ')
+    print('->', end= ' ')
+print('Acabou!')
