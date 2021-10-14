69581699e34ad4eb8007d5db96de2dae636ae962
felipecabraloliveira <felipecabral33@gmail.com>
2021-10-13 21:09:59 -0300

    Correção 048

--- curso-de-python-curso-em-video/scripts/exercicios/ex048.py
+++ curso-de-python-curso-em-video/scripts/exercicios/ex048.py
@@ -4,8 +4,14 @@
 # múltiplos de três e que se encontram no intervalo de 1 até 500.
 
 # Meu Código
+print('*' * 54)
+print(' ' * 6 + 'Números Ímpares e soma dos Múltiplos de 3!')
+print('*' * 54)
 soma = 0
+cont = 0 # Add na correção
 for c in range(1, 500, 2):
     if c % 3 == 0:
-        soma = soma + c
-print('A soma entre os multiplos de três de 1 a 500 foi {}'.format(soma))
+        soma += c
+        cont += 1
+print('A soma de todos os {} valores que são múltiplos de três de 1 a 500 foi {}'.format(cont, soma))
+# Correção - Ok
