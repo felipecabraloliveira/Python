76cfb7c9d998e3a5e0f1c740cae17a3842c61faf
felipecabraloliveira <felipecabral33@gmail.com>
2021-10-13 21:27:53 -0300

    Correção 050

--- curso-de-python-curso-em-video/scripts/exercicios/ex050.py
+++ curso-de-python-curso-em-video/scripts/exercicios/ex050.py
@@ -4,9 +4,15 @@
 # a soma apenas daqueles que forem pares. Se o valor digitado for ímpar, desconsidere-o.
 
 # Meu Código
+print('*' * 41)
+print(' ' * 6 + 'Soma números pares digitados!')
+print('*' * 41)
 soma = 0
+cont = 0  # Add na correção
 for c in range(1, 7):
     n = int(input('Digite o número {}: '.format(c)))
     if n % 2 == 0:
         soma += n
-print('A soma dos números pares digitados é {}'.format(soma))
+        cont += 1
+print('Você informou {} números pares e a soma deles é {}'.format(cont, soma))
+# Correção - Ok
