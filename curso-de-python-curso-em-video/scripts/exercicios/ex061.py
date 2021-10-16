e56db839cef5f1655fc03cd7cd22d14f4f841c1d
felipecabraloliveira <felipecabral33@gmail.com>
2021-10-16 16:10:04 -0300

    Exercício - 061

---
+++ curso-de-python-curso-em-video/scripts/exercicios/ex061.py
@@ -0,0 +1,18 @@
+# Exercício 061 do curso de Python - Curso em vídeo
+
+# Refaça o DESAFIO 51, lendo o primeiro termo e a razão de uma PA, mostrando os 10 primeiros
+# termos da progressão usando a estrutura while.
+
+# Meu Código
+print('*' * 32)
+print(' ' * 3 + 'Progressão Aritmética v2.0')
+print('*' * 32)
+pri = int(input('Primeiro termo: '))
+razao = int(input('Razão: '))
+dec = pri + (10 - 1) * razao
+cont = razao
+while pri < dec + razao:
+    print(pri, end=' ')
+    print('->', end=' ')
+    pri += razao
+print('Acabou!')
