f55ac841b41caa901cf557e32d946f4da7053d1e
felipecabraloliveira <felipecabral33@gmail.com>
2021-10-25 20:07:32 -0300

    Correção 061

--- curso-de-python-curso-em-video/scripts/exercicios/ex061.py
+++ curso-de-python-curso-em-video/scripts/exercicios/ex061.py
@@ -16,3 +16,16 @@ while pri < dec + razao:
     print('->', end=' ')
     pri += razao
 print('Acabou!')
+# Correção - OK
+
+print('-=' * 10)
+primeiro = int(input('Primeiro Termo: '))
+razao = int(input('Razão da PA: '))
+termo = primeiro
+
+cont = 1
+while cont <= 10:
+    print('{} -> '.format(termo), end='')
+    termo += razao
+    cont += 1
+print('Fim')
