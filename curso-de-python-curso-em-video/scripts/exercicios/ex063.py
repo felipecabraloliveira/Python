af69fa45d45385ead70ec19b2584fcbad2ca0a27
felipecabraloliveira <felipecabral33@gmail.com>
2021-10-25 21:06:35 -0300

    Exercício - 063

---
+++ curso-de-python-curso-em-video/scripts/exercicios/ex063.py
@@ -0,0 +1,21 @@
+# Exercício 063 do curso de Python - Curso em vídeo
+
+#  Escreva um programa que leia um número N inteiro qualquer e mostre
+#  na tela os N primeiros elementos de uma Sequência de Fibonacci
+
+# Meu Código
+print('*' * 33)
+print(' ' * 5 + 'Sequência de Fibonacci!')
+print('*' * 33)
+elemento = int(input('Digite quantos termos você quer que mostre: '))
+cont = 3
+ant = 0
+atu = 1
+print('{} - {}'.format(ant, atu), end='')
+while cont <= elemento:
+    prox = ant + atu
+    print(' - {}'.format(prox), end='')
+    ant = atu
+    atu = prox
+    cont += 1
+print(' -> Pausa')
