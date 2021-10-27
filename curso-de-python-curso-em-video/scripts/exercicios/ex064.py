ec2201d7a48cd09e3b588edb7e56a8d088a54bd2
felipecabraloliveira <felipecabral33@gmail.com>
2021-10-27 20:06:47 -0300

    Exercício - 064

---
+++ curso-de-python-curso-em-video/scripts/exercicios/ex064.py
@@ -0,0 +1,18 @@
+# Exercício 064 do curso de Python - Curso em vídeo
+
+# Crie um programa que leia vários números inteiros pelo teclado.
+# O programa só vai parar quando o usuário digitar o valor 999, que é a condição de parada.
+# No final, mostre quantos números foram digitados e qual foi a soma entre eles
+# (desconsiderando o flag).
+
+# Meu Código
+print('*' * 33)
+print(' ' * 5 + 'Tratando vários valores vs1.0')
+print('*' * 33)
+soma = 0
+num = 0
+while num != 999:
+    num = int(input('Digite um número ou [999 para sair]: '))
+    if num != 999:
+        soma += num
+print('A soma dos números é {}!'.format(soma))
