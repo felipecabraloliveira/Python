b8b34762754af73a5d615c7be2357eb9a39a364e
felipecabraloliveira <felipecabral33@gmail.com>
2021-10-27 20:09:13 -0300

    Exercício - 064

--- curso-de-python-curso-em-video/scripts/exercicios/ex064.py
+++ curso-de-python-curso-em-video/scripts/exercicios/ex064.py
@@ -9,10 +9,10 @@
 print('*' * 33)
 print(' ' * 5 + 'Tratando vários valores vs1.0')
 print('*' * 33)
-soma = 0
-num = 0
+cont = soma = num = 0
 while num != 999:
     num = int(input('Digite um número ou [999 para sair]: '))
     if num != 999:
         soma += num
-print('A soma dos números é {}!'.format(soma))
+        cont += 1
+print('A soma dos números é {} e foi digitado {} números!'.format(soma, cont))
