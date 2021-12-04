6f69cd6bb11f90fdd64000e9e12b9c04d59eeb9d
felipecabraloliveira <felipecabral33@gmail.com>
2021-12-04 19:43:13 -0300

    Correção 072

--- curso-de-python-curso-em-video/scripts/exercicios/ex072.py
+++ curso-de-python-curso-em-video/scripts/exercicios/ex072.py
@@ -12,8 +12,25 @@ print('=' * 37)
 contagem = ('Zero', 'Um', 'Dois', 'Três', 'Quatro', 'Cinco', 'Seis', 'Sete', 'Oito',
             'Nove', 'Dez', 'Onze', 'Doze', 'Treze', 'Quatorze', 'Quinze', 'Dezesseis',
             'Dezessete', 'Dezoito', 'Dezenove', 'Vinte')
-num = -1
+cont = ' '
 # Evitando que o número digitado esteja fora do range
-while num < 0 or num > 20:
+while True:
     num = int(input('Digite um número entre 0 e 20: '))
+    if num >= 0 and num <= 20:
+        print(f'Você digitou o número {contagem[num]}')
+        cont = str(input('Deseja Continuar? [S/N] ')).strip().upper()[0]
+    else:
+        print('Número incorreto, tente novamente. ', end='')
+    if cont == 'N':
+        break
+
+# Outra forma de chegar oo resultado
+print('\nOutro método!')
+while True:
+    num = int(input('Digite um número entre 0 e 20: '))
+    if 0 <= num <= 20:
+        break
+    print('Tente novamente. ', end='')
 print(f'Você digitou o número {contagem[num]}')
+
+# Correção - Ok
