fd59293b568034b67881bb4efcc90bc82e17348c
felipecabraloliveira <felipecabral33@gmail.com>
2022-03-26 12:35:16 -0300

    Exercício 083
    Exercício 083 do curso de Python do Curso em Vídeo.

---
+++ curso-de-python-curso-em-video/scripts/exercicios/ex083.py
@@ -0,0 +1,25 @@
+# Exercício 083 do curso de Python - Curso em vídeo
+
+# Crie um programa onde o usuário digite uma expressão qualquer que use parênteses.
+# Seu aplicativo deverá analisar se a expressão passada está com os
+# parênteses abertos e fechados na ordem correta.
+# Meu Código
+print('=' * 37)
+print(' ' * 4 + 'Validando expressões matemáticas')
+print('=' * 37)
+cores = {'blue': '\033[1;34m', 'red': '\033[1;31m', 'yellow': '\033[1;33m', 'g': '\033[1;32m',
+         'magenta': '\033[1;35m', 'peb': '\033[1;30;107m', 'bep': '\033[1;97;40m', 'remove': '\033[0;0m'}
+aberto = 0
+fechado = 0
+exp = str(input('Digite a expressão: ')).strip()
+for p, v in enumerate(exp):
+    if v == '(':
+        aberto += 1
+    elif v == ')':
+        fechado += 1
+print('=' * 37)
+if aberto == fechado:
+    print(f'A expressão é {cores["blue"]}Válida!{cores["remove"]}')
+else:
+    print(f'A expressão está {cores["red"]}Incorreta!{cores["remove"]}')
+print(f'Expressão: {exp}')
