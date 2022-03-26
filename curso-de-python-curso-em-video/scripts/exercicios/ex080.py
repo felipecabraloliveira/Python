8b8a92b95973a894af81da2179eaee6066b35d93
felipecabraloliveira <felipecabral33@gmail.com>
2022-03-26 11:41:59 -0300

    Exercício 080
    Exercício 080 do curso de Python do Curso em Vídeo.

---
+++ curso-de-python-curso-em-video/scripts/exercicios/ex080.py
@@ -0,0 +1,24 @@
+# Exercício 080 do curso de Python - Curso em vídeo
+
+# Crie um programa onde o usuário possa digitar cinco valores numéricos e
+# cadastre-os em uma lista, já na posição correta de inserção
+# (sem usar o sort()). No final, mostre a lista ordenada na tela.
+
+print('=' * 37)
+print(' ' * 4 + 'Lista ordenada sem repetições')
+print('=' * 37)
+valores = list()
+for c in range(0,5):
+    num = int(input('Digite um valor: '))
+    if c == 0 or num > valores[-1]:
+        valores.append(num)
+        print('Adicionado ao Final da lista')
+    else:
+        pos = 0
+        while pos < len(valores):
+            if num <= valores[pos]:
+                valores.insert(pos, num)
+                print(f'Adicionado na posição {pos}')
+                break
+            pos += 1
+print(f'Os valores digitados foram {valores}')
