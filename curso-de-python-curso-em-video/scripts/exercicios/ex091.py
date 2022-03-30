81a699873d652b5106adfccdcfcb7f515d7200cd
felipecabraloliveira <felipecabral33@gmail.com>
2022-03-30 17:31:15 -0300

    Correção 091
    Correção do exercício 091 do curso de Python do Curso em Vídeo.

--- curso-de-python-curso-em-video/scripts/exercicios/ex091.py
+++ curso-de-python-curso-em-video/scripts/exercicios/ex091.py
@@ -6,6 +6,7 @@
 # Meu Código
 from random import randint
 from time import sleep
+from operator import itemgetter
 print('=' * 37)
 print(' ' * 6 + 'Jogo de Dados em Python')
 print('=' * 37)
@@ -27,3 +28,20 @@ for i in sorted(jogador, key=jogador.get, reverse=True):
     print(' ' * 3, f'{cla}° Lugar: {i} com {jogador[i]} pontos.')
     cla += 1
 print('*' * 15, '   FIM   ', '*' * 14)
+# Correção - OK
+# outra forma
+print('\nOutra forma!')
+jogo = {'jogador1': randint(1, 6),
+        'jogador2': randint(1, 6),
+        'jogador3': randint(1, 6),
+        'jogador4': randint(1, 6),
+        }
+ranking = list()
+print('Valores sorteados: ')
+for k, v in jogo.items():
+    print(f'{k} tirou {v} no dado.')
+    sleep(1)
+ranking = sorted(jogo.items(), key=itemgetter(1), reverse=True)
+print('=' * 25)
+for i, v in enumerate(ranking):
+    print(f'{i+1}° lugar: {v[0]} com {v[1]}')
