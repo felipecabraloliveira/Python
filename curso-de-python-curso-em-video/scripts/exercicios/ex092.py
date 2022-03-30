4bace475bd52ade72cb5989d9a3f53d023880f7d
felipecabraloliveira <felipecabral33@gmail.com>
2022-03-30 18:31:42 -0300

    Exercício 092
    Exercício 092 do curso de Python do Curso em vídeo.

---
+++ curso-de-python-curso-em-video/scripts/exercicios/ex092.py
@@ -0,0 +1,28 @@
+# Exercício 092 do curso de Python - Curso em vídeo
+
+#  Crie um programa que leia nome, ano de nascimento e carteira de trabalho e cadastre-o
+#  (com idade) em um dicionário. Se por acaso a CTPS for diferente de ZERO,
+#  o dicionário receberá também o ano de contratação e o salário.
+#  Calcule e acrescente, além da idade, com quantos anos a pessoa vai se aposentar.
+# Meu Código
+from datetime import datetime
+print('=' * 37)
+print(' ' * 2 + 'Cadastro de Trabalhador em Python')
+print('=' * 37)
+
+trabalhador = dict()
+trabalhador['Nome'] = str(input('Nome: ').title().strip())
+nasc = int(input('Ano de Nascimento: '))
+trabalhador['Idade'] = datetime.now().year - nasc
+trabalhador['CTPS'] = int(input('Carteira de Trabalho (0 não tem): '))
+if trabalhador['CTPS'] != 0:
+    trabalhador['Contratação'] = int(input('Ano de contratação: '))
+    trabalhador['Salário'] = float(input('Salário: R$'))
+    # Corrigindo não é o ano, mas sim com que idade:
+    trabalhador['Aposentadoria'] = (trabalhador['Contratação'] + 35) - nasc
+    # Idade mínima considerando sexo masculino.
+    if trabalhador['Aposentadoria'] < 65:
+        trabalhador['Aposentadoria'] = 65
+print('-=' * 30)
+for i, v in trabalhador.items():
+    print(f'{i}: {v}')
